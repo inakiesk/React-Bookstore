be0d1db67c730adb045a66056a2e381b0cf50787
inakiesk <inakieskruiz@gmail.com>
2022-11-05 11:15:01 -0600

    Add Categories page with Navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './pages/components/Navbar';
 import Books from './pages/Books';
+import Categories from './pages/Categories';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Books />} />
+        <Route path="/Categories" element={<Categories />} />
       </Routes>
     </div>
   );
