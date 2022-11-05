570b12b4c3adbc3a37cccd85bad85ea4881f97a3
inakiesk <inakieskruiz@gmail.com>
2022-11-05 10:52:54 -0600

    Add components folder with basic structure for components

---
+++ src/pages/Books.js
@@ -0,0 +1,7 @@
+import BookList from './components/BookList';
+
+const Books = () => (
+  <BookList />
+);
+
+export default Books;
---
+++ src/pages/components/BookList.js
@@ -0,0 +1,9 @@
+import Book from './Book';
+
+const BookList = () => (
+  <div className="books">
+    <Book title="Dialogues" author="Plato" />
+  </div>
+);
+
+export default BookList;
