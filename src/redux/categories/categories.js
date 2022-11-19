6fddde392f2894e67967482493636e7b062b4121
inakiesk <inakieskruiz@gmail.com>
2022-11-18 21:09:45 -0600

    Add categories action and reducer

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,25 @@
+const initialState = [];
+
+
+// Action
+const CHECK_STATUS = 'CHECK_STATUS';
+
+// Action Creator
+const checkStatusAction = (payload) => ({
+  type: CHECK_STATUS,
+  payload,
+});
+
+// Reducer
+const categoriesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return "Under Construction";
+    default:
+      return state;
+  }
+
+};
+
+export {checkStatusAction}
+export default categoriesReducer;
