f44fc7c213fc6f2ac6ebea794d87da0a69a6be3b
inakiesk <inakieskruiz@gmail.com>
2022-11-19 15:02:01 -0600

    Refactor store with thunk and combine reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,19 @@
+import { combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { configureStore } from '@reduxjs/toolkit';
-import bookReducer from './books/books';
+import bookReducer, { fetchBooksAPI } from './books/books';
 import categoriesReducer from './categories/categories';
 
-const store = configureStore({
-  reducer: {
-    books: bookReducer,
-    categories: categoriesReducer,
-  },
+const rootReducer = combineReducers({
+  books: bookReducer,
+  categories: categoriesReducer,
 });
+
+const store = configureStore({
+  reducer: rootReducer,
+},
+applyMiddleware(thunk));
+
+store.dispatch(fetchBooksAPI());
 
 export default store;
