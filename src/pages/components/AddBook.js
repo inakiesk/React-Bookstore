4ed2d94e2737d31486c71c6f9a5deaea2947791e
inakiesk <inakieskruiz@gmail.com>
2022-11-19 09:41:38 -0600

    Add functionality for adding new books

--- src/pages/components/AddBook.js
+++ src/pages/components/AddBook.js
@@ -1,14 +1,44 @@
-const AddBook = () => (
-  <div className="add-section">
-    <h3 className="add-title"> Add New Book</h3>
-    <div className="add-form">
-      <input placeholder="Title" className="title-input" />
-      <input placeholder="Author" className="author-input" />
-      <button type="button" className="add-book">
-        Add Book
-      </button>
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBookAction } from '../redux/books/books';
+
+const AddBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+    };
+    dispatch(addBookAction(newBook));
+    setTitle('');
+    setAuthor('');
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Book Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          required
+        />
+        <button type="submit">Add Book</button>
+      </form>
     </div>
-  </div>
-);
+  );
+};
 
 export default AddBook;
